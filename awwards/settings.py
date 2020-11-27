71380d559273a8f34e508da3a31522c28b049f04
Josephshitandi <jsphshtnd@gmail.com>
2020-11-27 19:30:36 +0300

    Add registration templates

--- awwards/settings.py
+++ awwards/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tinymce',
+    'cloudinary',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
