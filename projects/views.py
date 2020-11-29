8d3792f567fa28ec282f848268fcca8ca11d7908
Josephshitandi <jsphshtnd@gmail.com>
2020-11-29 10:43:08 +0300

    Add a method that fetch all projects from database

--- projects/views.py
+++ projects/views.py
@@ -3,11 +3,13 @@ from django.http  import HttpResponse,Http404,HttpResponseRedirect
 import datetime as dt
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from .models import Projects,Profile
 
 # Create your views here.
 # @login_required(login_url='/accounts/register/')
 def index(request):
     date = dt.date.today()
     heading = "Welcome to Shitandi Awward Application"
+    projects = Projects.objects.all()
     
-    return render(request, 'home.html', {"date": date, "heading":heading})
+    return render(request, 'home.html', {"date": date, "heading":heading, "projects":projects})
